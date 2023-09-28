a24480eb7fad9b12088d2cc111156105c668fe3c
Starry Bei <hb794426422@gmail.com>
2023-09-28 18:32:29 +0800

    first commit

---
+++ Source/AutoWarriors/AutoWarriors.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AutoWarriors.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AutoWarriors, "AutoWarriors" );
+
+DEFINE_LOG_CATEGORY(LogAutoWarriors)
+ 
